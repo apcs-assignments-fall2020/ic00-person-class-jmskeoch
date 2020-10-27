2095c1fa3aa5f0ebe905764fba80fafe88f8244e
jmskeoch <jmskeoch@yahoo.com>
2020-10-27 16:07:59 +0000

    Basic Person class functionality

--- src/main/java/Person.java
+++ src/main/java/Person.java
@@ -1,5 +1,30 @@
 public class Person {
+    //Instance vars
+    public String firstName;
+    public String lastName;
+
+    //Constructor
+    public Person(String firstName, String lastName) 
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    //Non-static method
+    public String doSomething() 
+    {
+        String activity = "watching Netflix";
+        return "I am " + activity;
+    }
+
+    //To String method
+    @Override
+    public String toString() 
+    {
+        return "My name is " + this.firstName + " " + this.lastName + "!";
+    }
     
+
 }
 
 
--- src/main/java/PersonClient.java
+++ src/main/java/PersonClient.java
@@ -1,5 +1,7 @@
 public class PersonClient {
     public static void main(String[] args) {
-        
+        Person me = new Person("Jason", "Skeoch");
+        System.out.println(me.doSomething());
+        System.out.println(me);
     }
 }
